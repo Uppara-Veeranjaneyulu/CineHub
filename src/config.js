c445713b120f02702439ee9d2f02f8253ed42eca
Veeranji U2406 <uupparaveeranji@gmail.com>
2025-09-08 12:09:51 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+export const API_KEY = 'b856e799b2edcb353d0899b04e297d43';
 export const BASE_URL = 'https://api.themoviedb.org/3';
 export const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
